3a5d0383056b1db68ca041551b8bc1e26c1ea23f
talatizi kamel <kamel.talatizi@alumni.univ-avignon.fr>
2023-07-23 15:41:24 +0200

    Mise en place du CSS

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/style.css">
+    <script src="public/js/index.js">
+    </script>
+    <title>Todo</title>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <div class="logo"> Dyma todo</div>
+        </header>
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma Todo</h1>
+                <div class="todo-form"></div>
+                <div class="todo-list"></div>
+            </div>
+        </div>
+        <footer>
+            <p>©2001-2022 01copyright.fr. All right reserved.</p>
+        </footer>
+    </div>
+</body>
+
+</html>

be135b9053a623136e3864e44d5d1dcb3ab1eedb
talatizi kamel <kamel.talatizi@alumni.univ-avignon.fr>
2023-07-27 16:45:35 +0200

    sauvgarde et affichage des todos

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 const ERROR_REQUIRED = 'Veuillez renseigner une todo';
-const ERROR_TOO_SHORT = 'Veuillez entrer au moins 5 caractéres';
-
+const ERROR_TOO_SHORT = 'Veuillez entrer au moins 5 caractères';
+$filename = __DIR__ . "/data/todos.json";
 $error = '';
+$todos = [];
+
+if (file_exists($filename)) {
+    $data = file_get_contents($filename);
+    $todos = json_decode($data, true) ?? [];
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -13,8 +19,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } else if (strlen($todo) < 5) {
         $error = ERROR_TOO_SHORT;
     }
-}
 
+    if (!$error) {
+        $todos = [...$todos, [
+            'name' => $todo,
+            'done' => false,
+            'id' => time()
+        ]];
+        file_put_contents($filename, json_encode($todos));
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -45,7 +59,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <?php if ($error) : ?>
                     <p class="text-danger"><?= $error ?></p>
                 <?php endif ?>
-                <div class="todo-list"></div>
+                <ul class="todo-list">
+                    <?php foreach ($todos as $t) : ?>
+                        <li class="todo-item">
+                            <span class="todo-name"><?php echo $t['name'] ?></span>
+                            <button class="btn btn-primary">Valider</button>
+                            <button class="btn btn-danger">Supprimer</button>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
             </div>
         </div>
         <?php require_once 'include/footer.php'; ?>
